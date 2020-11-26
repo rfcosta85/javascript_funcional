6b38e9dad27bfa3b352e38d7b76e18b8a056b2b1
Rodrigo Costa <rfcosta85@gmail.com>
2020-11-26 15:47:40 -0300

    Aula 15 callback#01

---
+++ callback_1.js
@@ -0,0 +1,24 @@
+function exec(fn, a , b)
+{
+
+    return fn(a, b)    
+
+}
+
+/*const somarNoTerminal = (x, y) => console.log(x + y) 
+const subtrairNoTerminal = (w, z) => console.log(w - z)*/
+
+/*const somar = (x, y) => x + y
+const subtrair = (w, z) => w - z
+
+const somei =  exec(somar, 56, 38)
+const subtrai = exec(subtrair, 182, 27)
+
+console.log(somei)
+console.log(subtrai)*/
+
+setInterval(function() {
+
+    console.log('Exec...') 
+
+}, 1000)
