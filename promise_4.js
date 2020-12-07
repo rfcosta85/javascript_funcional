7c57333107599ff3dc04b1d8b522f7afbb56ae07
Rodrigo Costa <rfcosta85@gmail.com>
2020-12-07 19:26:04 -0300

    Aula 04 Promise

---
+++ promise_4.js
@@ -0,0 +1,48 @@
+function gerarNumerosEntre(min, max, tempo)
+{
+
+    if(min > max)
+    {
+
+        [max, min] = [min, max] // Temos aqui uma desestruturação quando atribuímos o valor do array utilizando o próprio array        
+
+    }
+
+    return new Promise(resolve => {
+
+        setTimeout(function(){
+
+            const fator = max - min + 1
+            const aleatorio = parseInt(Math.random() * fator) + min
+            resolve(aleatorio)
+
+        }, tempo)        
+
+    })
+}
+
+function gerarVariosNumeros()
+{
+
+    return Promise.all([
+
+        gerarNumerosEntre(1, 60, 4000),
+        gerarNumerosEntre(1, 60, 1000),
+        gerarNumerosEntre(1, 60, 500),
+        gerarNumerosEntre(1, 60, 3000),
+        gerarNumerosEntre(1, 60, 100),
+        gerarNumerosEntre(1, 60, 1500),
+
+    ])
+
+}
+
+console.time('promise')
+gerarVariosNumeros()
+    .then(numeros => console.log(numeros))
+    .then(() => {
+
+        console.timeLog('promise')
+        console.timeEnd('promise')
+
+    })
