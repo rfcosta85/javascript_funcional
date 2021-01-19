0115526d6c3645de31874d1d78fe360beb0f6607
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-19 11:28:18 -0300

    Aula 67 First Class Function

---
+++ funcional/67-firstClassFunction.js
@@ -0,0 +1,10 @@
+const x = 3
+const y = function(txt)
+{
+
+    return `Esse é o texto: ${txt}`
+
+}// assim conseguimos tratar uma função, da mesma forma que trataríamos uma variável. 
+
+console.log(x)
+console.log(y('olá'))
