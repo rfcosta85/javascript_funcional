88d0cfc275fa83c5026082cae2735a67ba6b3129
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-21 11:06:23 -0300

    aula 75- Composição de Funções 01

---
+++ funcional/composicao.js
@@ -0,0 +1,38 @@
+
+function composicao(...fns)
+{
+
+    return function(valor){
+
+        return fns.reduce((acc, fn) => {
+
+            return fn(acc)
+
+        }, valor)
+
+    }
+
+}
+
+function gritar(texto)
+{
+
+    return texto.toUpperCase()
+
+}
+
+function enfatizar(texto)
+{
+
+    return `${texto}!!!`
+
+}
+
+const resultado = composicao(
+
+    gritar,
+    enfatizar
+
+)('PARA')
+
+console.log(resultado)
