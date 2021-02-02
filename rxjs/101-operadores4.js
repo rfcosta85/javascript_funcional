fdf819151b80c5ca18762709753572fbd3fc2da0
Rodrigo Costa <rfcosta85@gmail.com>
2021-02-02 16:50:11 -0300

    Aula 101: Operadores 04

---
+++ rxjs/101-operadores4.js
@@ -0,0 +1,47 @@
+const { from, Observable, Subscriber } = require('rxjs')
+
+function primeiro()
+{
+
+    return function(source)
+    {
+        
+        return Observable.create(subscriber => {
+
+            source.subscribe({
+
+                next(v){
+
+                    subscriber.next(v)
+                    subscriber.complete()
+                }
+            })
+
+        })
+            
+    }
+}
+
+function ultimo()
+{
+
+    return function(source)
+    {
+
+        return Observable.create(subscriber => {
+
+            source.subscribe({
+
+                next(v){
+
+                    subscriber.next(v + 2000)
+                }
+            })
+        })
+    }
+}
+
+from([1, 2, 3, 4, 5])
+    //.pipe(primeiro())
+    .pipe(ultimo())
+    .subscribe(console.log)
