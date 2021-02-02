7706b7520c96d67b27ca7aa43e46933542783046
Rodrigo Costa <rfcosta85@gmail.com>
2021-02-02 16:35:08 -0300

    Aula 100- Operadores 03

---
+++ rxjs/100-operadores3.js
@@ -0,0 +1,26 @@
+const { Observable, Subscriber } = require('rxjs')
+
+function elementosComDelay(tempo, ...elementos)
+{
+
+    return Observable.create(subscriber => {
+
+        (elementos || []).forEach((el, i) => {
+
+            setTimeout(() => {
+
+                subscriber.next(el)
+
+                if(elementos.length === i + 1)
+                {
+
+                    subscriber.complete()
+                }
+
+            }, tempo * (i + 1)) 
+        })
+    })
+}
+
+elementosComDelay(1000, 1, 2, 3, 4, 5, 6)
+    .subscribe(console.log)
