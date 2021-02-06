7f26256a1f935aceb73f01acb5fc29c86678f2d0
Rodrigo Costa <rfcosta85@gmail.com>
2021-02-06 13:32:21 -0300

    Revisao Composicao de funcoes

---
+++ revisao/revisaoCOmposicaoDeFuncoes.js
@@ -0,0 +1,85 @@
+function composicao(...funcoes)
+{
+
+    return function(valor)
+    {
+
+        return funcoes.reduce((acumulado, funcao) => {
+
+            return funcao(acumulado)
+
+        }, valor)
+
+    }
+
+}
+
+function somar(numeroBase)
+{
+
+    return function(...numeros)
+    {
+        
+        let somandoNumeros = 0
+
+        for(let i = 0; i < numeros.length; i++)
+        {
+
+            somandoNumeros += numeros[i]
+
+        }
+
+        return numeroBase + somandoNumeros        
+
+    }    
+
+}
+
+function multiplicar(numeroBase)
+{
+
+    return function(...numeros)
+    {
+
+        let multiplicandoNumeros = 1
+
+        for(let i = 0; i < numeros[i]; i++)
+        {
+
+            multiplicandoNumeros *= numeros[i]
+
+        }
+
+        return  numeroBase * multiplicandoNumeros
+
+    }
+
+}
+
+const resultadoSoma = somar(2)(2, 2) 
+const resultadoMultiplicacao = multiplicar(1)(5, 2) 
+const somarResultados = resultadoSoma + resultadoMultiplicacao 
+
+function elevadoAoQuadrado(numero)
+{
+
+    return numero ** 2
+
+}
+
+function dividindoPorDois(numero)
+{
+
+    return numero / 2
+
+}
+
+const resultado = composicao(
+
+    elevadoAoQuadrado,
+    dividindoPorDois
+
+)(somarResultados)
+
+console.log(resultado) 
+
